bb4df63732baaddd47edf578e73c7698c6a79be2
lucasklaassen <lucasklaassen1@gmail.com>
2019-06-06 10:36:31 -0700

    refactor: update cart to have input of products

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from 'src/app/core/models/product.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {}
+export class CartComponent {
+  @Input() products: Product[] = [];
+
+  public totalPrice(): number {
+    return this.products.reduce((total, product) => total + product.price, 0);
+  }
+}
